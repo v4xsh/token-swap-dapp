232d83d5bf42dca7e63a0b7296828b106687d846
v4xsh <vkapoor0112@gmail.com>
2023-12-29 19:45:48 +0530

    Added Tx Details Tracker

--- src/pages/txhistory.tsx
+++ src/pages/txhistory.tsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import Layout from "@/components/Layout";
+import React, { useEffect, useState } from "react";
+import { useTransaction } from "wagmi";
 
 const txhistory = () => {
-  return (
-    <div>txhistory</div>
-  )
-}
+  const [hash, setHash] = useState("");
 
-export default txhistory
+  const { data, isError, isLoading } = useTransaction({
+    hash,
+  });
+
+  const [transactionState, setTransactionState] = useState({
+    blockHash: null,
+    blockNumber: null,
+    chainId: null,
+    from: null,
+    gas: null,
+    gasPrice: null,
+    to: null,
+  });
+
+  useEffect(() => {
+    if (data) {
+      setTransactionState((prevState) => ({
+        ...prevState,
+        blockHash: data?.blockHash?.toString(),
+        blockNumber: data?.blockNumber?.toString(),
+        chainId: data?.chainId?.toString(),
+        from: data?.from?.toString(),
+        gas: data?.gas?.toString(),
+        gasPrice: data?.gasPrice?.toString(),
+        to: data?.to?.toString(),
+      }));
+    console.log(data)
+    }
+  }, [data]);
+
+  return (
+    <Layout>
+      <div className="flex items-center justify-start flex-col gap-4">
+        <div className="flex items-center w-6/12">
+          <input
+            className="text-black px-5 py-2 text-base w-full"
+            placeholder="Enter Tx Hash"
+            type="text"
+            value={hash}
+            onChange={(e) => setHash(e.target.value.toString())}
+          />
+        </div>
+        <div>
+          {isLoading ? (
+            "Fetching details"
+          ) : (
+            <div className={`${isLoading ? "hidden" : "block"}`}>
+              <p>Block Hash: {transactionState.blockHash}</p>
+              <p>Block Number: {transactionState.blockNumber}</p>
+              <p>Chain ID: {transactionState.chainId}</p>
+              <p>From: {transactionState.from}</p>
+              <p>Gas: {transactionState.gas}</p>
+              <p>Gas Price: {transactionState.gasPrice}</p>
+              <p>To: {transactionState.to}</p>
+            </div>
+          )}
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default txhistory;
