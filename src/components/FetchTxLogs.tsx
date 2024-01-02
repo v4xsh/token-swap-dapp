17c11e6d4ff119c26669213702a31ffa06000de4
vansh kapoor <vkapoor0112@gmail.com>
2024-01-02 18:01:55 +0530

    Added Tx History logs limit upto 3000

--- src/components/FetchTxLogs.tsx
+++ src/components/FetchTxLogs.tsx
@@ -12,21 +12,22 @@ const FetchTxLogs = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
+        let latestBlock = await publicClient.getBlockNumber();
+        let fromBlock = latestBlock - 3000n;
         const logs = await publicClient.getContractEvents({
+          address: process.env.NEXT_PUBLIC_SUSD_ADDRESS as `0x${string}`,
           abi: Susd,
-          address: walletAddress,
-          // address: process.env.NEXT_PUBLIC_SUSD_ADDRESS as `0x${string}`,
           eventName: "Transfer",
-          // args: [
-          //   process.env.NEXT_PUBLIC_SUSD_ADDRESS as `0x${string}`,
-          //   walletAddress,
-          //   // 90000
-          //   100
-          // ]
+          args: {
+            from: walletAddress,
+            to: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS as `0x${string}`,
+          },
+          fromBlock: fromBlock,
+          toBlock: latestBlock,
         });
         console.log(logs);
-        const hashes = logs.map((log) => log.address);
-        // setTxns(hashes);
+        // const hashes = logs.map((log) => log.address);
+        setTxns(logs);
       } catch (err) {
         console.error(err);
       }
