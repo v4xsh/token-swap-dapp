d259d16585104c3c3f5d5e82b0a5a6fc6023ac70
vansh kapoor <vkapoor0112@gmail.com>
2024-01-04 17:01:06 +0530

    type fixing

--- src/components/FetchTxLogs.tsx
+++ src/components/FetchTxLogs.tsx
@@ -4,6 +4,7 @@ import Usdc from "../../abi/usdc.json";
 import { publicClient } from "@/pages/_app";
 import { useTokenStore } from "../../store/useTokenStore";
 import { batchRequest } from "../../utils/batchRequest";
+import { type Abi } from "viem";
 
 type TransactionType = {
   transactionHash: string;
@@ -46,7 +47,7 @@ const FetchTxLogs: React.FC = () => {
             contractAddress,
             susdAddress,
             walletAddress as "0x${string}",
-            Susd
+            Susd as Abi
           );
           usdcTx = await batchRequest(
             batchSize,
@@ -56,22 +57,21 @@ const FetchTxLogs: React.FC = () => {
             contractAddress,
             usdcAddress,
             walletAddress as "0x${string}",
-            Usdc
+            Usdc as Abi
           );
           swapContractBlock += BigInt(batchSize) * blockDifference;
           swapContractBlockTo += BigInt(batchSize) * blockDifference;
         }
 
-        if (susdTx.length) {
-          susdTx=susdTx.map((tx) => ({ ...tx, operation: "Swap A To B" }));
+        if (susdTx && susdTx.length) {
+          susdTx = susdTx.map((tx) => ({ ...tx, operation: "Swap A To B" }));
         }
-        if (usdcTx.length) {
-          usdcTx=usdcTx.map((tx) => ({ ...tx, operation: "Swap B To A" }));
+        if (usdcTx && usdcTx.length) {
+          usdcTx = usdcTx.map((tx) => ({ ...tx, operation: "Swap B To A" }));
         }
 
-        console.log(susdTx, usdcTx);
-
-        txnsArray = [...susdTx, ...usdcTx];
+        if (usdcTx) txnsArray = [...usdcTx];
+        if (susdTx) txnsArray = [...susdTx];
       } catch (err) {
         console.error(err);
       }
@@ -84,39 +84,40 @@ const FetchTxLogs: React.FC = () => {
   return (
     <div className="mx-12 mt-10">
       <div className="">
-        {txns.map((tx: TransactionType) => {
-          return (
-            <div
-              className="border-b-2 border-zinc-500 py-10 px-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-auto gap-4"
-              key={tx?.transactionHash}
-            >
-              <div className="py-2 px-4 border border-zinc-500 rounded-xl">
-                <span className="me-2 font-bold">Address</span>
-                {tx?.address}
+        {txns &&
+          txns.map((tx: TransactionType) => {
+            return (
+              <div
+                className="border-b-2 border-zinc-500 py-10 px-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-auto gap-4"
+                key={tx?.transactionHash}
+              >
+                <div className="py-2 px-4 border border-zinc-500 rounded-xl">
+                  <span className="me-2 font-bold">Address</span>
+                  {tx?.address}
+                </div>
+                <div className="py-2 px-4 border border-zinc-500 rounded-xl">
+                  <span className="me-2 font-bold">Hash</span>
+                  {tx?.transactionHash}
+                </div>
+                <div className="py-2 px-4 border border-zinc-500 rounded-xl">
+                  <span className="me-2 font-bold">From</span>
+                  {tx?.args?.from}
+                </div>
+                <div className="py-2 px-4 border border-zinc-500 rounded-xl">
+                  <span className="me-2 font-bold">To</span>
+                  {tx?.args?.to}
+                </div>
+                <div className="py-2 px-4 border border-zinc-500 rounded-xl">
+                  <span className="me-2 font-bold">Tokens</span>
+                  {tx?.args?.value.toString()}
+                </div>
+                <div className="py-2 px-4 border border-zinc-500 rounded-xl">
+                  <span className="me-2 font-bold">Operation</span>
+                  {tx?.operation.toString()}
+                </div>
               </div>
-              <div className="py-2 px-4 border border-zinc-500 rounded-xl">
-                <span className="me-2 font-bold">Hash</span>
-                {tx?.transactionHash}
-              </div>
-              <div className="py-2 px-4 border border-zinc-500 rounded-xl">
-                <span className="me-2 font-bold">From</span>
-                {tx?.args?.from}
-              </div>
-              <div className="py-2 px-4 border border-zinc-500 rounded-xl">
-                <span className="me-2 font-bold">To</span>
-                {tx?.args?.to}
-              </div>
-              <div className="py-2 px-4 border border-zinc-500 rounded-xl">
-                <span className="me-2 font-bold">Tokens</span>
-                {tx?.args?.value.toString()}
-              </div>
-              <div className="py-2 px-4 border border-zinc-500 rounded-xl">
-                <span className="me-2 font-bold">Operation</span>
-                {tx?.operation.toString()}
-              </div>
-            </div>
-          );
-        })}
+            );
+          })}
       </div>
     </div>
   );
