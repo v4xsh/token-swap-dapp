fc3ef512bc9b0e007fb9da3265856df580b8c8d0
Vansh Kapoor <vkapoor0112@gmail.com>
2024-01-15 01:53:40 +0530

    Test case #1 (Minting working now)

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -3,7 +3,7 @@ const DOMAIN: string = 'http://localhost:3000';
 describe('Synpress Demo', () => {
   it('should connect to MetaMask and display wallet address', () => {
     cy.setupMetamask(
-      process.env.SECRET_KEY,
+      'dynamic broom defense diamond dragon busy naive avoid arch junk endless myself',
       // 'battle raccoon helmet please deliver keep kiss round orphan frame update message',
       {
         networkName: 'Goerli test network',
@@ -29,23 +29,9 @@ describe('Synpress Demo', () => {
 
     cy.visit(DOMAIN + '/mint');
     cy.get('#mintUsdcInput').type('10');
+    cy.wait(1000);
     cy.get('#mintUsdc').click();
-    cy.get('#mintUsdc').click();
-    cy.get('#mintUsdc').click();
-    cy.get('#mintUsdc').click();
-    cy.wait(7000);
-    // cy.isMetamaskWindowActive().should("be.true");
     cy.confirmMetamaskTransaction();
-    // cy.confirmMetamaskPermissionToSpend('10').should('be.true');
-
-
-    cy.visit(DOMAIN + '/');
-    cy.get('#swapBToA').type('1000');
-    // cy.get('#submitSwapBToA').click();
-    // cy.confirmMetamaskPermissionToSpend().should('be.true');
-
-    cy.wait(30000000);
-
-    // cy.get('#disconnect-btn').click();
+    cy.wait(30000);
   });
 });
