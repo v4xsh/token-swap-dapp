736d3ca6f12f920c31cc3778525c1c1959955915
Vansh Kapoor <vkapoor0112@gmail.com>
2024-01-16 01:34:21 +0530

    Fixed EsLint Errors on App.jsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,28 @@
-import React from "react";
+import React from 'react';
 
-import "@/styles/globals.css";
+import '@/styles/globals.css';
 
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
 
-import { configureChains, WagmiConfig, createConfig } from "wagmi";
-import { goerli } from "wagmi/chains";
-import { publicProvider } from "wagmi/providers/public";
-import { MetaMaskConnector } from "wagmi/connectors/metaMask";
-import { createPublicClient, http } from "viem";
+import { WagmiConfig, createConfig } from 'wagmi';
+import { goerli } from 'wagmi/chains';
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
+import { createPublicClient, http } from 'viem';
 
 // export const { publicClient } = createPublicClient([goerli], [publicProvider()]);
 export const publicClient = createPublicClient({
   chain: goerli,
-  transport: http(),
+  transport: http()
 });
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App ({
+  Component,
+  pageProps
+}: AppProps): React.JSX.Element {
   const config = createConfig({
     autoConnect: true,
     publicClient,
-    connectors: [new MetaMaskConnector()],
+    connectors: [new MetaMaskConnector()]
   });
 
   return (
